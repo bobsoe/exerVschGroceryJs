daa32cbff93dde8ff7ca8db4696410d73e738603
bobsoe <77603595+bobsoe@users.noreply.github.com>
2021-11-21 22:27:32 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const shopper = {
+    firstName: "Joe",
+    lastName : "A",
+    age : 40,
+    isFemale: false,
+    grocreyCart : ["fruit", "veggies", "milk", "pasta", "meat", "icecream" ]
+};
+
+function ShowShopper() {
+    alert( "Today's shopper is " + shopper.firstName + " " + shopper.lastName)
+}
+
+console.log(shopper);
+
